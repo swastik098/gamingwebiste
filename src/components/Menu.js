3be8fccc1fb0dad758740890e47635b9754ff5e8
Swastik Giri <swastikrancodes@gmail.com>
2023-05-01 15:20:16 +0530

    Header Footer added

---
+++ src/components/Menu.js
@@ -0,0 +1,33 @@
+export const Menu = [
+    {
+       text:'Home',
+       path: '/',
+       className : 'menu-data'
+    },
+    {
+        text:'Gallery',
+        path : '/gallery',
+        className : 'menu-data'
+     },
+     {
+        text:'Sign In',
+        path : '/signin',
+        className : 'menu-data'
+     },
+     {
+        text:'Sign Up',
+        path:'signup',
+        className : 'menu-data'
+     },
+     {
+        text:'About',
+        path:'/about',
+        className : 'menu-data'
+     },
+     {
+        text:'History',
+        path:'/history',
+        className : 'menu-data'
+     },
+
+]
