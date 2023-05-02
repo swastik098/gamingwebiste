8870682b2088427897fb4c5f4c56f2c6c6d1cf00
Swastik Giri <swastikrancodes@gmail.com>
2023-05-02 17:44:19 +0530

    slider added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Slider from "./slider";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="slider">
+        <Slider />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
