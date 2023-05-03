489cff6da6b5e9b40362c18206014e2b25a00063
Swastik Giri <swastikrancodes@gmail.com>
2023-05-03 17:07:21 +0530

    home page changes

--- src/components/Home/slider.js
+++ src/components/Home/slider.js
@@ -1,80 +1,61 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useState } from "react";
+// import "./Slider.scss";
+// import slide_image_1 from "../../assets/gameimg1.jpg";
 
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slides = [
+    {
+      image:
+        "https://i.pinimg.com/564x/01/02/ea/0102ea2768a9c06ce53710dcb7064a27.jpg",
+      title: "G A M I N G",
+      subtitle: "Slider Subtitle 1",
+    },
+    {
+      image:
+        "https://i.pinimg.com/564x/bb/8e/9f/bb8e9f9b156565f5246fe172495d6a70.jpg",
+      title: "Slider Title 2",
+      subtitle: "Slider Subtitle 2",
+    },
+    {
+      image:
+        "https://i.pinimg.com/564x/27/54/5d/27545d423364ad4f4f992c99fc43754b.jpg",
+      title: "Slider Title 3",
+      subtitle: "Slider Subtitle 3",
+    },
+  ];
 
-import { EffectCoverflow, Pagination, Navigation } from "swiper";
+  const handlePrev = () => {
+    setCurrentSlide(currentSlide === 0 ? slides.length - 1 : currentSlide - 1);
+  };
 
-import slide_image_1 from "../../assets/gameimg1.jpg";
-import slide_image_2 from "../../assets/gameimg1.jpg";
-import slide_image_3 from "../../assets/gameimg1.jpg";
-import slide_image_4 from "../../assets/gameimg1.jpg";
-import slide_image_5 from "../../assets/gameimg1.jpg";
-import slide_image_6 from "../../assets/gameimg1.jpg";
-import slide_image_7 from "../../assets/gameimg1.jpg";
+  const handleNext = () => {
+    setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1);
+  };
 
-function Slider() {
   return (
-    <div className="container">
-      {/* <h1 className="heading"></h1> */}
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
-        }}
-        pagination={{ el: ".swiper-pagination", clickable: true }}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
-      >
-        <SwiperSlide>
-          <img src={slide_image_1} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_2} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_3} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_4} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_5} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_6} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_7} alt="slide_image" />
-        </SwiperSlide>
-
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
+    <div className="slider">
+      {slides.map((slide, index) => (
+        <div
+          key={index}
+          className={`slider-item ${index === currentSlide ? "active" : ""}`}
+        >
+          <img src={slide.image} alt={`Image ${index}`} />
+          <div className="slider-text">
+            <h2>{slide.title}</h2>
+            <p>{slide.subtitle}</p>
           </div>
-          <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
-          </div>
-          <div className="swiper-pagination"></div>
         </div>
-      </Swiper>
+      ))}
+      <button className="prev-btn" onClick={handlePrev}>
+        Prev
+      </button>
+      <button className="next-btn" onClick={handleNext}>
+        Next
+      </button>
     </div>
   );
-}
+};
 
 export default Slider;
