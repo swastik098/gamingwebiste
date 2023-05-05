026f3346bd30ad6c3beee5664046ad2969aa94b3
Lalit <lalitsoren58@gmail.com>
2023-05-05 18:52:10 +0530

    tellwind-intro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header, { HeaderPhone } from "./components/Headers";
 import Home from "./components/Home/Home";
 import GameDevelopment from "./components/GameDevelopement/GameDevelopment";
 import GameArt from "./components/GameArt/GameArt";
@@ -8,13 +8,14 @@ import Publishing from "./components/Publishing/Publishing";
 import Animation from "./components/Animation/Animation";
 import Academy from "./components/Academy/Academy";
 import Footer from "./components/Footer";
+import Navigation from "./components/Headers";
+import Contact from "./components/Home/Contact";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <Router>
-      <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gamedevelopment" element={<GameDevelopment />} />
@@ -22,6 +23,7 @@ function App() {
         <Route path="/animation" element={<Animation />} />
         <Route path="/publishing" element={<Publishing />} />
         <Route path="/academy" element={<Academy />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
--- src/components/Home/ImageGallery.js
+++ src/components/Home/ImageGallery.js
@@ -2,11 +2,18 @@ import React from "react";
 
 const ImageGallery = ({ images }) => {
   return (
-    <div className="image-gallery">
-      <div className="row">
+    <div className="image-gallery w-1/2">
+      <div className="flex flex-wrap -mx-2">
         {images.map((image, index) => (
-          <div key={index} className="col-4">
-            <img src={image.url} alt={image.alt} />
+          <div
+            key={index}
+            className="w-full md:w-1/2 lg:w-1/3 px-2 py-2 hover:h-full"
+          >
+            <img
+              src={image.url}
+              alt={image.alt}
+              className="w-full h-24 object-cover"
+            />
           </div>
         ))}
       </div>
