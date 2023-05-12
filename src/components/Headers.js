3dea3a8981eed2f894f4e8f743ad907f7b922e4e
Lalit <lalitsoren58@gmail.com>
2023-05-12 11:43:47 +0530

    headerfottercolor

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -8,95 +8,95 @@ function Navigation() {
     setShowMenu(false);
   };
 
-  const navStyle = {
-    position: "sticky",
-    top: 0,
-    left: 0,
-    width: "100%",
-    zIndex: 9999,
-  };
-
   return (
-    <nav
-      style={navStyle}
-      className="flex  font-sans-Roboto items-center justify-between flex-wrap bg-slate-800 p-8"
-    >
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">
-          <NavLink to="/" onClick={closeMenu}>
-            Vault Games Studio
-          </NavLink>
-        </span>
-      </div>
-      <div className="block lg:hidden">
-        <button
-          className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white"
-          onClick={() => setShowMenu(!showMenu)}
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path
-              d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"
-              fill="#ffffff"
-            />
-          </svg>
-        </button>
-      </div>
-      <div
-        className={`${
-          showMenu ? "block" : "hidden"
-        } w-full block flex-grow lg:flex lg:items-center lg:w-auto`}
-      >
-        <div className="text-2xl text-center space-x-20 lg:flex-grow">
-          <NavLink
-            to="/gamedevelopment"
-            className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
-            onClick={closeMenu}
-          >
-            Game Development
-          </NavLink>
-          <NavLink
-            to="/gameart"
-            className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
-            onClick={closeMenu}
-          >
-            Game Art
-          </NavLink>
-          <NavLink
-            to="/animation"
-            className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
-            onClick={closeMenu}
-          >
-            Animation
-          </NavLink>
-          <NavLink
-            to="/publishing"
-            className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
-            onClick={closeMenu}
-          >
-            Publishing
-          </NavLink>
-          <NavLink
-            to="/academy"
-            className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
-            onClick={closeMenu}
-          >
-            Academy
+    <div className=" px-10 bg-gradient-to-r from-yellow-400 to-red-500">
+      <nav className="container mx-auto flex flex-wrap items-center justify-between py-5">
+        <div className="flex items-center flex-shrink-0 mr-6">
+          <NavLink className="text-white font-bold text-2xl" to="/">
+            Logo
           </NavLink>
         </div>
-        <NavLink
-          href="/contact"
-          className="inline-block text-xl px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-800 hover:bg-white mt-4 lg:mt-0"
-          onClick={closeMenu}
+        <div className="block lg:hidden">
+          <button
+            className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            <svg
+              className="h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                strokeWidth="2"
+                d="M4 6h12M4 10h12M4 14h12"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`${
+            showMenu ? "block" : "hidden"
+          } w-full lg:flex lg:items-center lg:w-auto`}
         >
-          Get in Touch
-        </NavLink>
-      </div>
-    </nav>
+          <div className="text-xl lg:flex-grow text-center lg:text-left">
+            <NavLink
+              to="/gamedevelopment"
+              className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mx-4"
+              onClick={closeMenu}
+            >
+              Game Development
+            </NavLink>
+            <NavLink
+              to="/gameart"
+              className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mx-4"
+              onClick={closeMenu}
+            >
+              Game Art
+            </NavLink>
+            <NavLink
+              to="/animation"
+              className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mx-4"
+              onClick={closeMenu}
+            >
+              Animation
+            </NavLink>
+            <NavLink
+              to="/publishing"
+              className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mx-4"
+              onClick={closeMenu}
+            >
+              Publishing
+            </NavLink>
+            <NavLink
+              to="/academy"
+              className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mx-4"
+              onClick={closeMenu}
+            >
+              Academy
+            </NavLink>
+          </div>
+          <div
+            className={`${
+              showMenu ? "block" : "hidden"
+            } w-full lg:flex lg:items-center lg:w-auto`}
+          >
+            <div className="text-2xl lg:flex-grow text-center lg:text-left">
+              <NavLink
+                href="/contact"
+                className="inline-block text-xl px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-800 hover:bg-white mt-4 lg:mt-0"
+                onClick={closeMenu}
+              >
+                Get in Touch
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
