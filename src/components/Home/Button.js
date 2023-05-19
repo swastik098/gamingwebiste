45710421fafa5c43ff51a9706e9266f1cf3cd475
Abinash <abinash.tripathy@speridian.com>
2023-05-19 23:25:25 +0530

    publish form issues fixed

--- src/components/Home/Button.js
+++ src/components/Home/Button.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Button = ({ text, onClick, className }) => {
+const Button = ({ text, onClick, className} ,props) => {
+  console.log(props,"type")
   return (
     <button
       className={`text-2xl  bg-gradient-to-b from-blue to-white-500 hover:bg-blue-950 text-black font-bold py-3 px-4 rounded-lg ${className}`}
       onClick={onClick}
+      // type={type}
     >
       {text}
     </button>
--- src/components/Publishing/TopSection.js
+++ src/components/Publishing/TopSection.js
@@ -1,6 +1,6 @@
 import React from "react";
 import video from "../../assets/video-game-7249.mp4";
-
+import RegisterForm from "./RegisterPublish";
 const SkewedDesign = () => {
   return (
     <div className="relative">
@@ -11,7 +11,7 @@ const SkewedDesign = () => {
         muted
         loop
       />
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center">
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center absolute top-0 left-0 w-1/2 h-full">
         <h2 className="text-4xl md:text-8xl md:text-center font-bold mb-2 text-white">
           GAME
           <span className="text-blue "> PUBLISHING</span>
@@ -21,7 +21,34 @@ const SkewedDesign = () => {
           games into chart-topping hits
         </p>
       </div>
+      <div className="absolute absolute top-0 right-0 w-1/2 h-full">
+      <RegisterForm />
+      </div>
     </div>
+//     <div className="relative">
+//        <video
+//         className="h-screen w-full object-cover"
+//         src={video}
+//         autoPlay
+//         muted
+//         loop
+//       />
+//     <div class="flex flex-wrap">
+//   <div class="w-full md:w-1/2">
+//   <h2 className="text-4xl md:text-8xl md:text-center font-bold mb-2 text-white absolute ">
+//            GAME
+//            <span className="text-blue "> PUBLISHING</span>
+//          </h2>
+//          <p className="text-lg md:text-3xl  text-white text-center pt-5 absolute ">
+//            <span className="text-blue">Vault Games Studio</span> turn mobile
+//            games into chart-topping hits
+//          </p>
+//   </div>
+//   <div class="w-full md:w-1/2">
+//     <RegisterForm />
+//   </div>
+// </div>
+// </div>
   );
 };
 
