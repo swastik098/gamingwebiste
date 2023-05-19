e852047ce128137e05e1dfa2c79a95b30bfba38c
Abinash <abinash.tripathy@speridian.com>
2023-05-19 23:27:11 +0530

    publish form issues fixed

--- src/components/Home/Button.js
+++ src/components/Home/Button.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Button = ({ text, onClick, className} ,props) => {
-  console.log(props,"type")
   return (
     <button
       className={`text-2xl  bg-gradient-to-b from-blue to-white-500 hover:bg-blue-950 text-black font-bold py-3 px-4 rounded-lg ${className}`}
