f99a1a6913f88dca7e455ae10872a10be5797671
Lalit <lalitsoren58@gmail.com>
2023-05-28 13:29:12 +0530

    mobile responsive publishing , academy, gamedev talk to us

--- src/components/Publishing/Publishing.js
+++ src/components/Publishing/Publishing.js
@@ -4,26 +4,30 @@ import RegisterForm from "./RegisterPublish";
 
 const Publishing = () => {
   return (
-    <div className="relative">
-      <video
-        className="h-screen w-full object-cover"
-        src={video}
-        autoPlay
-        muted
-        loop
-      />
-      <div className="absolute inset-0 z-40 flex flex-col justify-center items-center text-center top-0 left-0 mb-72 md:mb-96 md:left-20 w-full md:w-1/2 h-full font-sans">
-        <h2 className="text-2xl md:text-4xl lg:text-8xl md:text-center font-bold mb-2 text-white">
-          GAME
-          <span className="text-blue font-sans"> PUBLISHING</span>
-        </h2>
-        <p className="text-base md:text-lg lg:text-3xl text-white text-center pt-5 pb-10 md:pb-20">
-          Turn Mobile and PC Games Into Chart-Topping Hits
-        </p>
+    <div className="">
+      <div className="relative">
+        <video
+          className="h-screen w-full object-cover"
+          src={video}
+          autoPlay
+          muted
+          loop
+        />
       </div>
+      <div className="mt-50">
+        <div className="absolute  inset-0 z-40 flex flex-col justify-center items-center text-center top-0 left-0 mb-72 md:mb-96 md:left-20 w-full md:w-1/2 h-full font-sans">
+          <h2 className="text-2xl md:text-4xl lg:text-8xl md:text-center font-bold mb-2 text-white">
+            GAME
+            <span className="text-blue font-sans"> PUBLISHING</span>
+          </h2>
+          <p className="text-base md:text-lg lg:text-3xl text-white text-center pt-5 pb-10 md:pb-20">
+            Turn Mobile and PC Games Into Chart-Topping Hits
+          </p>
+        </div>
 
-      <div className="flex flex-grow relative md:absolute md:bg-inherit bg-black top-0 right-0 w-full h-full p-12 md:flex md:w-1/2">
-        <RegisterForm />
+        <div className="flex flex-grow relative md:absolute md:bg-inherit bg-black top-0 right-0 w-full h-full p-12 md:flex md:w-1/2">
+          <RegisterForm />
+        </div>
       </div>
     </div>
   );
